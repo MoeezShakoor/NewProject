0f1c014e7b0d59ce96730132d93e7832198acea3
Moeez Shakoor <moeez.shakoor@regbits.com>
2020-04-16 13:32:19 +0500

    initial commit

---
+++ assets/JS/TableSearch.js
@@ -0,0 +1,93 @@
+var x = window.matchMedia("(min-width: 1150px)")
+ myFunction(x) // Call listener function at run time
+ x.addListener(myFunction) // Attach listener function on state changes
+
+ function myFunction(x) {
+   if (x.matches) { // If media query matches
+      //console.log(x);
+      var NavLEFT= document.getElementById("NavLeft");
+      NavLEFT.style.visibility= "visible";
+      NavLEFT.style.width= "22%";
+      NavLEFT.style.marginTop = "10px";
+
+      var Innercontainer= document.getElementById("InnerContainer");
+      Innercontainer.style.visibility= "visible";
+      Innercontainer.style.width= "74%";
+
+      var Downarrow= document.getElementById("DownArrow");
+      Downarrow.style.visibility= "hidden";
+
+
+      var Rightarrow= document.getElementById("RightArrow");
+      Rightarrow.style.visibility= "hidden";
+      } 
+      else {
+      var NavLEFT= document.getElementById("NavLeft");
+      NavLEFT.style.visibility= "hidden";
+      NavLEFT.style.width= "0px";
+
+      var Innercontainer= document.getElementById("InnerContainer");
+      Innercontainer.style.visibility= "visible";
+      Innercontainer.style.width= "88%";
+
+      var Downarrow= document.getElementById("DownArrow");
+      Downarrow.style.visibility= "visible";
+
+
+      
+   }
+ }
+ 
+ 
+
+
+function DEL(){
+    var ConfirmBox= document.getElementById("Confirmation");
+    ConfirmBox.style.visibility= "visible";
+
+}
+function Del_Confirm_Yes(){
+      var ConfirmBox= document.getElementById("Confirmation");
+      ConfirmBox.style.visibility= "hidden";
+
+}
+function Del_Confirm_No(){
+   var ConfirmBox= document.getElementById("Confirmation");
+   ConfirmBox.style.visibility= "hidden";
+
+}
+function showNavBar(){
+  var Innercontainer= document.getElementById("InnerContainer");
+   Innercontainer.style.visibility= "hidden";
+   Innercontainer.style.width= "0px";
+
+   var Downarrow= document.getElementById("DownArrow");
+   Downarrow.style.visibility= "hidden";
+
+
+   var Rightarrow= document.getElementById("RightArrow");
+   Rightarrow.style.visibility= "visible";
+
+   var NavLEFT= document.getElementById("NavLeft");
+   NavLEFT.style.visibility= "visible";
+   NavLEFT.style.width= "90%";
+   NavLEFT.style.marginTop = "40px";
+}
+
+function closeNavBar(){
+   var Innercontainer= document.getElementById("InnerContainer");
+   Innercontainer.style.visibility= "visible";
+   Innercontainer.style.width= "88%";
+
+   var Downarrow= document.getElementById("DownArrow");
+   Downarrow.style.visibility= "visible";
+
+
+   var Rightarrow= document.getElementById("RightArrow");
+   Rightarrow.style.visibility= "hidden";
+
+   var NavLEFT= document.getElementById("NavLeft");
+   NavLEFT.style.visibility= "hidden";
+   NavLEFT.style.width= "0px";
+  
+ }
---
+++ assets/JS/VuePractise.js
@@ -0,0 +1,148 @@
+Vue.component('product',{
+	template:`
+		<div>
+			<div class="gridbox">	
+				<div>
+					<img :src="image" class="sockImage">
+				</div>
+				<div class="grid-2">
+					<h2>{{itemName}}</h2>
+					
+					<br>
+					<p v-if="Stock"> In Stock</p>
+					<p v-else> Out of Stock</p>
+					<ul>
+						<li v-for="detail in details">{{detail}}</li>
+					</ul>
+					<div v-for="(variant,index) in variants" 
+					:key="variant.variantID"
+					class="colorbox"
+					:style="{backgroundColor: variant.variantColor }"
+					v-on:mouseover="updatecolor(index)">
+					</div>
+
+					<button v-on:click="addToCart">Add to cart</button>
+					
+				</div>
+			</div>
+			<hr />
+
+			<div class="reviewbox">
+			<h2>Reviews</h2>
+			<br><br>
+			<p v-if="!reviews.length"> there are no reviews</p>
+			<ul>
+				<li v-for="review in reviews">
+				<p>{{review.name}}</p>
+				<p>{{review.review}}</p>
+				</li>
+			</ul>
+			</div>
+			<hr />
+			
+			<product-review @reviewSumbit="addReview"></product-review>
+			
+		</div>
+	`,
+	data(){
+		return {
+			Name:'Socks',
+			brand:'branded',
+			// image:'./assets/Images/greenSocks.jpg',
+			selectedIndex:0,
+			reviews:[],
+			cart: 0,
+			details:["80% Cotton","20% polyester"],
+			variants: [
+			{
+				variantID:1,
+				variantColor:"green",
+				inventory:10,
+				variantImage:'./assets/Images/greenSocks.jpg'
+				
+			},
+			{
+				variantID:2,
+				variantColor:"black",
+				inventory:0,
+				variantImage:'./assets/Images/blackSocks.jpg'			}
+			]
+		}
+	} ,
+	methods:{
+		updatecolor(index){
+			this.selectedIndex=index
+		},
+		addToCart(){
+			this.$emit('add-to-cart')
+		},
+		addReview(productreview){
+			this.reviews.push(productreview)
+		}
+	},
+	computed:{
+		itemName(){
+			return this.brand +' '+ this.Name
+		},
+		image(){
+			return this.variants[this.selectedIndex].variantImage
+		},
+		Stock(){
+			return this.variants[this.selectedIndex].inventory
+		}
+	}
+	
+})
+
+
+
+Vue.component('product-review',{
+	template:`
+	<form class="formarea" @submit.prevent="onsubmit">
+		<h3>Add a Review</h3>
+		<hr />
+		<p>
+			<label for="name">name: </label>
+			<input id="name" v-model="name">
+		</p>
+		<p>
+			<label for="review">review: </label>
+			<textarea id="review" v-model="review"></textarea>
+		</p>
+		<p>
+			<input type="submit" value="submit">
+		</p>
+	</form>
+	`,
+	data(){
+		return{
+			name:null,
+			review:null
+		}
+	},
+	methods:{
+		onsubmit(){
+			let productreview={
+				name: this.name,
+				review: this.review
+			}
+			this.$emit('reviewSumbit',productreview)
+			this.name=null
+			this.review=null
+		}
+	}
+})
+
+
+
+var Socks = new Vue({
+	el:'#Socks',
+	data:{
+		cart:0
+	},
+	methods:{
+		updatecart(){
+			this.cart +=1
+		}
+	}
+})
